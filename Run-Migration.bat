809cfdc3695afcff94f87c664c2f99bf3cf07e5b
Ahmad Jber <ahmad.jber2001@gmail.com>
2024-05-27 11:57:38 +0300

    run-migration.bat
    this a file that update the database after make or create a migration

---
+++ Run-Migration.bat
@@ -0,0 +1,1 @@
+dotnet ef database update %2 --project .\%1.EntityFrameworkCore\%1.EntityFrameworkCore.csproj  --startup-project .\%1.Web.Host\%1.Web.Host.csproj --context %1.EntityFrameworkCore.%1DbContext --configuration Debug 
